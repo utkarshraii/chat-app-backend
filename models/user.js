f286c45c2deb628b2a64e6d529f52d3cfe5bc0cc
utkarshraii <utkarshr348@gmail.com>
2023-07-13 23:53:33 +0530

    2nd
    user schema made with mongoose fns

---
+++ models/user.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "First Name is required"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last Name is required"],
+  },
+  avatar: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    validate: {
+      validator: function (email) {
+        return String(email)
+          .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+      },
+      message: (props) => `Email (${props.value}) is invalid!`,
+    },
+  },
+  password: {
+    type: String,
+  },
+  passwordChangedAt: {
+    type: Date,
+  },
+
+  passwordResetToken: {
+    type: String,
+  },
+
+  passwordResetExpires: {
+    type: Date,
+  },
+
+  createdAt: {
+    type: Date,
+  },
+  updatedAt: {
+    type: Date,
+  },
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword, //123456
+  userPassword //fnwrknfew
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = new mongoose.model("User", userSchema);
+module.exports = User;
