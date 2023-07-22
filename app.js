62aeb609f75feced03270deb23b510a5572baba5
utkarshraii <utkarshr348@gmail.com>
2023-07-22 13:54:31 +0530

    updated app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const morgan = require("morgan"); // HTTP request logger middleware for node.js
 const routes = require("./routes/index");
 const rateLimit = require("express-rate-limit"); // Basic rate-limiting middleware for Express. Use to limit repeated requests to public APIs and/or endpoints such as password reset.
 const helmet = require("helmet"); // Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
-
+const AppError = require("./utils/AppError");
 // These headers are set in response by helmet
 
 // Content-Security-Policy: default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests
@@ -46,10 +46,6 @@ app.use(
   cors({
     origin: ["https://chat-app-eta-six.vercel.app", "http://localhost:3000"],
 
-    methods: ["GET", "PATCH", "POST", "DELETE", "PUT"],
-
-    credentials: true, //
-
     //   Access-Control-Allow-Credentials is a header that, when set to true , tells browsers to expose the response to the frontend JavaScript code. The credentials consist of cookies, authorization headers, and TLS client certificates.
   })
 );
@@ -58,8 +54,8 @@ app.use(cookieParser());
 
 // Setup express response and body parser configurations
 app.use(express.json({ limit: "10kb" })); // Controls the maximum request body size. If this is a number, then the value specifies the number of bytes; if it is a string, the value is passed to the bytes library for parsing. Defaults to '100kb'.
-app.use(bodyParser.json()); // Returns middleware that only parses json
-app.use(bodyParser.urlencoded({ extended: true })); // Returns middleware that only parses urlencoded bodies
+app.use(express.urlencoded({ extended: true })); // Returns middleware that only parses urlencoded bodies
+app.use(cookieParser()); // Returns middleware that only parses json
 
 app.use(
   session({
@@ -98,5 +94,9 @@ app.use(mongosanitize());
 app.use(xss());
 
 app.use(routes);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 module.exports = app;
